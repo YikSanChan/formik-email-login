56413e84fc137c90d23cc52b286c5b70aaff2777
yiksanchan <evan.chanyiksan@gmail.com>
2018-10-25 03:08:46 -0400

    Add newsletter and plan fields

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { withFormik, Form, Field } from "formik";
 
-const App = ({ values, handleChange, handleSubmit }) => (
+const App = ({ values }) => (
   <Form>
     <Field type="email" name="email" placeholder="Email" />
     <Field type="password" name="password" placeholder="Password" />
+    <label>
+      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
     <button type="submit">Submit</button>
   </Form>
 );
 
 const FormikApp = withFormik({
-  mapPropsToValues: ({ email, password }) => ({
+  mapPropsToValues: ({ email, password, newsletter, plan }) => ({
     email: email || "",
-    password: password || ""
+    password: password || "",
+    newsletter: newsletter || false,
+    plan: plan || "free"
   }),
   handleSubmit: values => {
     console.log(values);
